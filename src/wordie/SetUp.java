73c8bdf299cc4c360bb6594df81ebab443ba0c86
MaddiLynnMay <MaddiLynnMay@gmail.com>
2025-07-24 01:36:07 +0100

    Initial code

---
+++ src/wordie/SetUp.java
@@ -0,0 +1,12 @@
+package wordie;
+
+public class SetUp {
+	//choose random category -- maybe through an ENUM?
+	
+	//choose a random word from that category
+		//set main word to that word to be compared to later
+	
+	//reset the wordie GUI (implement the reset button to do this when it is pressed)
+	
+	
+}
---
+++ src/wordie/wordieCheck.java
@@ -0,0 +1,17 @@
+package wordie;
+
+public class wordieCheck {
+	//count what line we are on
+	
+	//check selected word against input word
+	
+	//fill color and word into gui
+	
+	//check for loss / display loss message
+	
+	//check for win / display win message
+	
+	
+	
+	
+}
