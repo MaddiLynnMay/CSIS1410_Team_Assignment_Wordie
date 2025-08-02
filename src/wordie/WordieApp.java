011b46d436ba78ef88ba803073b7d0eb92200095
enwanarkanin <enwan.arkanin@hotmail.com>
2025-08-02 15:48:56 -0600

    Update WordieApp.java

--- src/wordie/WordieApp.java
+++ src/wordie/WordieApp.java
@@ -9,6 +9,7 @@ package wordie;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -23,6 +24,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -40,6 +42,8 @@ public class WordieApp extends JFrame {
 	private int guessCount = 0;
 	private JLabel[][] rowArray;
 	private JComboBox<Topics> topicSelector;
+	private JButton confirm;
+	private JLabel chosenTopic;
 
 	/**
 	 * Launch the application and have the frame centered.
@@ -74,16 +78,18 @@ public class WordieApp extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setTitle("Wordie - Guess the Word!");
 
-		topicBox();
+		topicBox(); // contains topic drop-down, and displays chosen topic
 
-		controlPanel();
+		controlPanel(); // contains the submit and reset buttons
 
-		gameBox();
+		gameBox(); // contains array for columns/rows, user text field, KeyListner and
+					// ActionListener
 
 	}
 
 	/**
-	 * This is for the center part or "main" part of the game. Players field.
+	 * This is for the center part or "main" part of the game. Players field. This
+	 * has 5 rows and 5 columns for letters and a user field.
 	 */
 	private void gameBox() {
 		JPanel playingField = new JPanel();
@@ -93,137 +99,21 @@ public class WordieApp extends JFrame {
 		// Set layout to 6 rows x 5 columns
 		playingField.setLayout(new GridLayout(6, 5, 5, 5));
 
-		/**
-		 * Row A
-		 */
-		JLabel A1 = new JLabel("", JLabel.CENTER);
-		A1.setOpaque(true);
-		A1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(A1);
-		JLabel A2 = new JLabel("", JLabel.CENTER);
-		A2.setOpaque(true);
-		A2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(A2);
-		JLabel A3 = new JLabel("", JLabel.CENTER);
-		A3.setOpaque(true);
-		A3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(A3);
-		JLabel A4 = new JLabel("", JLabel.CENTER);
-		A4.setOpaque(true);
-		A4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(A4);
-		JLabel A5 = new JLabel("", JLabel.CENTER);
-		A5.setOpaque(true);
-		A5.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(A5);
+		rowArray = new JLabel[5][5];
 
-		/**
-		 * Row B
-		 */
-		JLabel B1 = new JLabel("", JLabel.CENTER);
-		B1.setOpaque(true);
-		B1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(B1);
-		JLabel B2 = new JLabel("", JLabel.CENTER);
-		B2.setOpaque(true);
-		B2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(B2);
-		JLabel B3 = new JLabel("", JLabel.CENTER);
-		B3.setOpaque(true);
-		B3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(B3);
-		JLabel B4 = new JLabel("", JLabel.CENTER);
-		B4.setOpaque(true);
-		B4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(B4);
-		JLabel B5 = new JLabel("", JLabel.CENTER);
-		B5.setOpaque(true);
-		B5.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(B5);
+		// Loop to create 5 rows with 5 columns each. This is where each letter will go.
+		for (int row = 0; row < 5; row++) {
+			for (int col = 0; col < 5; col++) {
+				JLabel label = new JLabel("", JLabel.CENTER);
+				label.setOpaque(true);
+				label.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				playingField.add(label);
+				rowArray[row][col] = label;
+			}
+		}
 
-		/**
-		 * Row C
-		 */
-		JLabel C1 = new JLabel("", JLabel.CENTER);
-		C1.setOpaque(true);
-		C1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(C1);
-		JLabel C2 = new JLabel("", JLabel.CENTER);
-		C2.setOpaque(true);
-		C2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(C2);
-		JLabel C3 = new JLabel("", JLabel.CENTER);
-		C3.setOpaque(true);
-		C3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(C3);
-		JLabel C4 = new JLabel("", JLabel.CENTER);
-		C4.setOpaque(true);
-		C4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(C4);
-		JLabel C5 = new JLabel("", JLabel.CENTER);
-		C5.setOpaque(true);
-		C5.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(C5);
+		// Empty box section 1 to keep input box centered
 
-		/**
-		 * Row D
-		 */
-		JLabel D1 = new JLabel("", JLabel.CENTER);
-		D1.setOpaque(true);
-		D1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(D1);
-		JLabel D2 = new JLabel("", JLabel.CENTER);
-		D2.setOpaque(true);
-		D2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(D2);
-		JLabel D3 = new JLabel("", JLabel.CENTER);
-		D3.setOpaque(true);
-		D3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(D3);
-		JLabel D4 = new JLabel("", JLabel.CENTER);
-		D4.setOpaque(true);
-		D4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(D4);
-		JLabel D5 = new JLabel("", JLabel.CENTER);
-		D5.setOpaque(true);
-		D5.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(D5);
-
-		/**
-		 * Row E
-		 */
-		JLabel E1 = new JLabel("", JLabel.CENTER);
-		E1.setOpaque(true);
-		E1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(E1);
-		JLabel E2 = new JLabel("", JLabel.CENTER);
-		E2.setOpaque(true);
-		E2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(E2);
-		JLabel E3 = new JLabel("", JLabel.CENTER);
-		E3.setOpaque(true);
-		E3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(E3);
-		JLabel E4 = new JLabel("", JLabel.CENTER);
-		E4.setOpaque(true);
-		E4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(E4);
-		JLabel E5 = new JLabel("", JLabel.CENTER);
-		E5.setOpaque(true);
-		E5.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		playingField.add(E5);
-
-		JLabel[] rowA = { A1, A2, A3, A4, A5 };
-		JLabel[] rowB = { B1, B2, B3, B4, B5 };
-		JLabel[] rowC = { C1, C2, C3, C4, C5 };
-		JLabel[] rowD = { D1, D2, D3, D4, D5 };
-		JLabel[] rowE = { E1, E2, E3, E4, E5 };
-
-		rowArray = new JLabel[][] { rowA, rowB, rowC, rowD, rowE };
-
-		/**
-		 * Empty box section 1 to keep input box centered
-		 */
 		JLabel emptyBox1 = new JLabel();
 		emptyBox1.setHorizontalAlignment(SwingConstants.CENTER);
 		playingField.add(emptyBox1);
@@ -231,9 +121,8 @@ public class WordieApp extends JFrame {
 		emptyBox2.setHorizontalAlignment(SwingConstants.CENTER);
 		playingField.add(emptyBox2);
 
-		/**
-		 * Input your words here
-		 */
+		// Text field for the user.
+
 		txtEnterWordHere = new JTextField();
 		txtEnterWordHere.setText("Enter word here");
 		txtEnterWordHere.setToolTipText("Enter a word to see if you got the guess correct or partially correct");
@@ -243,12 +132,19 @@ public class WordieApp extends JFrame {
 		txtEnterWordHere.setForeground(Color.DARK_GRAY);
 		txtEnterWordHere.setHorizontalAlignment(SwingConstants.CENTER);
 		playingField.add(txtEnterWordHere);
-		txtEnterWordHere.setColumns(10);
 
-		// This makes it so only letters and backspace can be added to the text field.
-		// This also makes it so all the letters get capitalized so it looks nicer and
-		// is more uniform.
+		// When user types, it removes the"Enter word here"
+
 		txtEnterWordHere.addKeyListener(new java.awt.event.KeyAdapter() {
+			@Override
+			public void keyPressed(java.awt.event.KeyEvent e) {
+				if (txtEnterWordHere.getText().equals("Enter word here")) {
+					txtEnterWordHere.setText("");
+				}
+			}
+
+			// Only capital letters and backspace can be added to the text field
+
 			@Override
 			public void keyTyped(java.awt.event.KeyEvent e) {
 				char c = e.getKeyChar();
@@ -261,39 +157,27 @@ public class WordieApp extends JFrame {
 			}
 		});
 
-		// When the user clicks in the text field, the text disappears so the user can
-		// start typing their word. The text reappears when the user is no longer in the
-		// text field.
+		// Pressing Enter on the keyboard acts like clicking Submit.
 
-		txtEnterWordHere.addFocusListener(new java.awt.event.FocusAdapter() {
+		txtEnterWordHere.addActionListener(new ActionListener() {
 			@Override
-			public void focusGained(java.awt.event.FocusEvent e) {
-				txtEnterWordHere.setText("");
-
-			}
-
-			@Override
-			public void focusLost(java.awt.event.FocusEvent e) {
-				if (txtEnterWordHere.getText().isEmpty()) {
-					txtEnterWordHere.setText("Enter word here");
-				}
+			public void actionPerformed(ActionEvent e) {
+				confirm.doClick();
 			}
 		});
 
-		/**
-		 * Empty box section 2 to keep input box centered
-		 */
+		// Empty box section 2 to keep input box centered
+
 		JLabel emptyBox3 = new JLabel();
 		emptyBox3.setHorizontalAlignment(SwingConstants.CENTER);
 		playingField.add(emptyBox3);
 		JLabel emptyBox4 = new JLabel();
 		emptyBox4.setHorizontalAlignment(SwingConstants.CENTER);
 		playingField.add(emptyBox4);
-
 	}
 
 	/**
-	 * Section for the reset and submit button.
+	 * Section for the reset and submit buttons.
 	 */
 	private void controlPanel() {
 		JPanel buttonPanel = new JPanel();
@@ -301,13 +185,14 @@ public class WordieApp extends JFrame {
 		contentPane.add(buttonPanel, BorderLayout.SOUTH);
 		{
 			// This is the submit button.
-			JButton confirm = new JButton("Submit");
-			confirm.addMouseListener(new MouseAdapter() {
+			confirm = new JButton("Submit");
+			confirm.addActionListener(new ActionListener() {
 				@Override
-				public void mouseClicked(MouseEvent e) {
+				public void actionPerformed(ActionEvent e) {
 
 					String userGuessedWord = txtEnterWordHere.getText();
 					Topics selectedTopic = (Topics) topicSelector.getSelectedItem();
+
 					// if the user hasn't chosen a topic yet, it will prompt the user to select one.
 
 					if (selectedTopic == null) {
@@ -316,18 +201,20 @@ public class WordieApp extends JFrame {
 					}
 
 					String actualWord = setUp.getSelectedWord();
-					// each time the user adds a word, the guess count goes up one.
-					// But if the user doesn't add a five letter word, either too many or too few
-					// letters, the guess count won't go up and instead the user will be prompted to
-					// add a five letter word.
+					// Checks the word entered is a five-letter word.
+					// Guess count goes up one if valid five-letter word.
+
 					if (wordieCheck.letterCountCheck(userGuessedWord)) {
 						wordieCheck.compareWords(userGuessedWord, actualWord, rowArray[guessCount], guessCount);
 						guessCount++;
 					} else {
 						JOptionPane.showMessageDialog(WordieApp.this, "Please enter a 5-letter word.");
 					}
+					txtEnterWordHere.setText("Enter word here");
+					txtEnterWordHere.requestFocusInWindow();
 				}
 			});
+
 			confirm.setBackground(new Color(0, 123, 255));
 			confirm.setForeground(Color.WHITE);
 			confirm.setOpaque(true);
@@ -339,9 +226,10 @@ public class WordieApp extends JFrame {
 			// This is the reset button.
 			JButton restart = new JButton("Reset");
 			restart.addMouseListener(new MouseAdapter() {
+
 				@Override
 				public void mouseClicked(MouseEvent e) {
-					// resets the guess count so the user can replay
+					// resets the guess count
 					guessCount = 0;
 					// resets the letters and background
 					for (JLabel[] row : rowArray) {
@@ -354,9 +242,10 @@ public class WordieApp extends JFrame {
 					// resets the user text field and drop-down
 					txtEnterWordHere.setText("Enter word here");
 					topicSelector.setSelectedItem(null);
-					topicSelector.setEnabled(true);
-
+					topicSelector.setVisible(true);
+					chosenTopic.setVisible(false);
 				}
+
 			});
 			restart.setBackground(Color.GRAY);
 			restart.setForeground(Color.WHITE);
@@ -368,7 +257,10 @@ public class WordieApp extends JFrame {
 	}
 
 	/**
-	 * Topic box
+	 * This is for the Topic box at the top of the game. It has a JComboBox to
+	 * display the drop-down options. When the user chooses the topic, the JCombobox
+	 * goes invisible and instead displays the chosen topic.
+	 * 
 	 */
 	private void topicBox() {
 		JPanel topics = new JPanel();
@@ -382,12 +274,47 @@ public class WordieApp extends JFrame {
 		{
 			// this is the drop-down option for the topic
 			topicSelector = new JComboBox<>();
+
+			chosenTopic = new JLabel("");
+			chosenTopic.setForeground(Color.WHITE);
+			chosenTopic.setFont(new Font("Segoe UI Historic", Font.PLAIN, 20));
+			chosenTopic.setVisible(false);
+			topics.add(chosenTopic);
+
 			topicSelector.addItem(null);
 			for (Topics t : Topics.values()) {
 				topicSelector.addItem(t);
 			}
 
-			// chooses the word from the topic. Once topic is selected, user can't change it
+			// This is a setRenderer to change the placeholder in the drop-down to "Choose your topic"
+			topicSelector.setRenderer(new javax.swing.ListCellRenderer<Topics>() {
+
+				@Override
+				public Component getListCellRendererComponent(JList<? extends Topics> list, Topics value, int index,
+						boolean isSelected, boolean cellHasFocus) {
+
+					JLabel label;
+
+					if (value == null) {
+						label = new JLabel("Choose Your Topic");
+					} else {
+						label = new JLabel(value.name());
+					}
+
+					if (isSelected) {
+						label.setBackground(Color.LIGHT_GRAY);
+						label.setOpaque(true);
+					} else {
+						label.setBackground(Color.WHITE);
+						label.setOpaque(true);
+					}
+
+					return label;
+				}
+			});
+
+			// When topic is chosen by the user, the chosen topic is displayed
+
 			topicSelector.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
@@ -395,7 +322,9 @@ public class WordieApp extends JFrame {
 					if (selected != null) {
 						try {
 							setUp.loadTopicWords(selected);
-							topicSelector.setEnabled(false);
+							topicSelector.setVisible(false);
+							chosenTopic.setText(selected.name());
+							chosenTopic.setVisible(true);
 						} catch (IOException ex) {
 							ex.printStackTrace();
 						}
